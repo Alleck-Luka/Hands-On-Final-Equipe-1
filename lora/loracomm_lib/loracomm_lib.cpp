49884f8b25d23a7deb122274a0e3cc5a48ee18b3
androvisck <androvisck@gmail.com>
2026-09-18 19:04:27 -0400

    tentando corrigir pasta

---
+++ lora/loracomm_lib/loracomm_lib.cpp
@@ -0,0 +1,95 @@
+#include "loracomm_lib.h"
+
+using namespace std; // Permite usar string, ifstream diretamente ao invés de std::string
+
+namespace devtitans::loracomm
+{ // Entra no pacote devtitans::loracomm
+
+  int Loracomm::connect()
+  {
+    char dirPath[] = "/sys/kernel/loracomm";
+    struct stat dirStat;
+    if (stat(dirPath, &dirStat) == 0)
+      if (S_ISDIR(dirStat.st_mode))
+        return 1; // Se o diretório existir, retorna 1
+  }
+
+  string Loracomm::readFileValue(string file)
+  {
+    int connected = this->connect();
+
+    if (connected == 1)
+    { // Conectado. Vamos solicitar o valor ao dispositivo
+      string value;
+      string filename = string("/sys/kernel/loracomm/") + file;
+      ifstream file(filename); // Abre o arquivo do módulo do kernel
+
+      if (file.is_open())
+      {                       // Verifica se o arquivo foi aberto com sucesso
+        getline(file, value); // Lê uma linha do arquivo
+        file.close();
+        return value;
+      }
+    }
+
+    // Se chegou aqui, não foi possível conectar ou se comunicar com o dispositivo
+    return "";
+  }
+
+  bool Loracomm::writeFileValue(string file, string value)
+  {
+    int connected = this->connect();
+
+    if (connected == 1)
+    { // Conectado. Vamos solicitar o valor ao dispositivo
+      string filename = string("/sys/kernel/loracomm/") + file;
+      ofstream file(filename, ios::trunc); // Abre o arquivo limpando o seu conteúdo
+
+      if (file.is_open())
+      {                // Verifica se o arquivo foi aberto com sucesso
+        file << value; // Escreve no arquivo
+        file.close();
+        return true;
+      }
+    }
+
+    // Se chegou aqui, não foi possível conectar ou se comunicar com o dispositivo
+    return false;
+  }
+
+  bool Loracomm::send(string payload)
+  {
+    return this->writeFileValue("send", payload);
+  }
+
+  bool Loracomm::ping()
+  {
+    return stoi(this->readFileValue("ping"));
+  }
+
+  bool Loracomm::getAux()
+  {
+    return stoi(this->readFileValue("aux"));
+  }
+
+  long Loracomm::getRxCount()
+  {
+    return stol(this->readFileValue("rx_count"));
+  }
+
+  string Loracomm::getLastRx()
+  {
+    return this->readFileValue("last_rx");
+  }
+
+  string Loracomm::getKey()
+  {
+    return this->readFileValue("key");
+  }
+
+  bool Loracomm::setKey(string key)
+  {
+    return this->writeFileValue("key", key);
+  }
+
+}
